df8bb7ffc967fc5c1e91ea8bcb818caecf7748ab
Jay Fredlund <jayfredlund@gmail.com>
2020-08-31 14:26:35 -0700

    fix alias_method_chain

--- app/controllers/fitter_happier_controller.rb
+++ app/controllers/fitter_happier_controller.rb
@@ -1,15 +1,15 @@
 class FitterHappierController < ActionController::Base
   layout nil
-  
+
   def index
     render(:text => "FitterHappier Site Check Passed\n")
   end
-  
+
   def site_check
     time = Time.now.to_formatted_s(:rfc822)
     render(:text => "FitterHappier Site Check Passed @ #{time}\n")
   end
-  
+
   def site_and_database_check
     table_name = (Rails::VERSION::STRING >= '2.1.0' ? 'schema_migrations' : 'schema_info')
     query      = "SELECT max(lpad(version, 20, 0)) FROM #{table_name}"
@@ -17,14 +17,15 @@ class FitterHappierController < ActionController::Base
     time       = Time.now.to_formatted_s(:rfc822)
     render(:text => "FitterHappier Site and Database Check Passed @ #{time}\nSchema Version: #{version}\n")
   end
-  
+
   private
-  
+
   def process_with_silence(*args)
     logger.quietly do
       process_without_silence(*args)
     end
   end
- 
-  alias_method_chain :process, :silence
+
+  alias_method :process_without_silence, :process
+  alias_method :process, :process_with_silence
 end
