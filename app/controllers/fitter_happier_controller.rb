97b032927f87abad8be1c6c09f6754cf8a3040c2
Jay Fredlund <jayfredlund@gmail.com>
2020-08-31 15:09:31 -0700

    updates site check methods to work with updated versions

--- app/controllers/fitter_happier_controller.rb
+++ app/controllers/fitter_happier_controller.rb
@@ -6,15 +6,15 @@ class FitterHappierController < ActionController::Base
   end
 
   def site_check
-    time = Time.now.to_formatted_s(:rfc822)
+    time = Time.now.rfc822
     render(:text => "FitterHappier Site Check Passed @ #{time}\n")
   end
 
   def site_and_database_check
     table_name = (Rails::VERSION::STRING >= '2.1.0' ? 'schema_migrations' : 'schema_info')
-    query      = "SELECT max(lpad(version, 20, 0)) FROM #{table_name}"
+    query      = "SELECT max(lpad(version, 20)) FROM #{table_name}"
     version    = ActiveRecord::Base.connection.select_value(query).to_i
-    time       = Time.now.to_formatted_s(:rfc822)
+    time       = Time.now.rfc822
     render(:text => "FitterHappier Site and Database Check Passed @ #{time}\nSchema Version: #{version}\n")
   end
 
